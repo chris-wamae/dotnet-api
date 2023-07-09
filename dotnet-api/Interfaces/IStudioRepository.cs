ae2c34da8a686cd08c5180bd6a7d2b1c0487aa72
chris-wamae <chriswamae123@gmail.com>
2023-07-09 07:59:11 +0300

    created Studio  Interface

---
+++ dotnet-api/Interfaces/IStudioRepository.cs
@@ -0,0 +1,19 @@
+using dotnet_api.Models;
+
+namespace dotnet_api.Repository{
+   
+    public interface IStudioRepository
+    {
+      ICollection<Studio> GetStudios();
+      Studio GetStudioById(int Id);
+      Studio GetStudioByName(string name);
+      bool StudioExists(int Id);
+
+
+
+      
+
+    }
+}
+
+
--- dotnet-api/Repository/StudioRepository.cs
+++ dotnet-api/Repository/StudioRepository.cs
@@ -4,39 +4,39 @@ using dotnet_api.Models;
 
 namespace dotnet_api.Repository
 {
-  
-  public class StudioRepository : IStudioRepository
-  {
-    private readonly DataContext _context;
 
-    public StudioRepository(DataContext context)
+    public class StudioRepository : IStudioRepository
     {
-        _context = context;
+        private readonly DataContext _context;
+
+        public StudioRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool StudioExists(int studioId)
+        {
+            return _context.Studios.Any(s => s.Id == studioId);
+        }
+
+        public Studio GetStudioById(int studioId)
+        {
+            return _context.Studios.Where(s => s.Id == studioId).FirstOrDefault();
+        }
+
+        public Studio GetStudioByName(string name)
+        {
+            return _context.Studios.Where(s => s.Name == name).FirstOrDefault();
+        }
+
+        public ICollection<Studio> GetStudios()
+
+        {
+
+            return _context.Studios.OrderBy(s => s.Id).ToList();
+
+        }
+
     }
-
-    public bool StudioExists(int studioId)
-    {
-        return _context.Studios.Any(s => s.Id == studioId);
-    }
-
-    public Studio GetStudioById(int studioId)
-    {
-        return _context.Studios.Where(s => s.Id == studioId).FirstOrDefault();
-    }
-
-    public Studio GetStudioByName(string name)
-    {
-        return _context.Studios.Where(s => s.Name == name).FirstOrDefault();
-    }
-
-    public ICollection<Studio> GetStudios()
-    
-    {
-
-     return _context.Studios.OrderBy(s => s.Id).ToList();
-
-    }
-
-  }
 
 }

fb98f9e4f784ca0faa36fbe8196124e7825f1024
chris-wamae <chriswamae123@gmail.com>
2023-07-09 08:02:53 +0300

    Created Studio Dto

---
+++ dotnet-api/Dto/StudioDto.cs
@@ -0,0 +1,12 @@
+namespace dotnet_api.Dto
+{
+    public class StudioDto
+    {
+        public int Id {get;set;}
+        
+        public string Name {get; set;}
+
+        public DateTime CreationDay {get;set;}
+
+    }
+}
--- dotnet-api/Interfaces/IStudioRepository.cs
+++ dotnet-api/Interfaces/IStudioRepository.cs
@@ -1,18 +1,14 @@
 using dotnet_api.Models;
 
-namespace dotnet_api.Repository{
-   
+namespace dotnet_api.Repository
+{
+
     public interface IStudioRepository
     {
-      ICollection<Studio> GetStudios();
-      Studio GetStudioById(int Id);
-      Studio GetStudioByName(string name);
-      bool StudioExists(int Id);
-
-
-
-      
-
+        ICollection<Studio> GetStudios();
+        Studio GetStudioById(int Id);
+        Studio GetStudioByName(string name);
+        bool StudioExists(int Id);
     }
 }
 

4ad7ad3c0a9bce77de99983dd14ce68f1e69483c
chris-wamae <chriswamae123@gmail.com>
2023-07-09 07:10:18 +0300

    created platform Controller

---
+++ dotnet-api/Controllers/PlatformController.cs
@@ -0,0 +1,59 @@
+using dotnet_api.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_api.Models;
+using AutoMapper;
+using dotnet_api.Dto;
+
+namespace dotnet_api.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+
+  public class PlatformController : Controller 
+  {
+    private readonly IPlatformRepository _platformRepository;
+    private readonly IMapper _mapper;
+
+    public PlatformController(IPlatformRepository platformRepository, IMapper mapper)
+    {
+        _platformRepository = platformRepository;
+        _mapper = mapper; 
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Platform>))]
+
+    public IActionResult GetPlatforms()
+    {
+    var platforms = _mapper.Map <List <PlatformDto>>(_platformRepository.GetPlatforms());
+
+    if(!ModelState.IsValid)
+
+    return BadRequest(ModelState);
+
+    return Ok(platforms);
+
+    }
+
+    [HttpGet("{gameId}")]
+    [ProducesResponseType(200, Type = typeof(Platform))]
+    [ProducesResponseType(400)]
+
+    public IActionResult GetPlatform(int platformId)
+     {
+        if(!_platformRepository.PlatformExists(platformId))
+        return NotFound();
+        
+        var platform = _mapper.Map<PlatformDto>(_platformRepository.GetPlatformById(platformId));
+
+        if(!ModelState.IsValid)
+          return BadRequest(ModelState);
+
+        return Ok(platform);
+        
+     }
+
+
+
+  }
+}

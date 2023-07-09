aa63af4e08fd6cf18e91c71ed38b9dc06bc1cb98
chris-wamae <chriswamae123@gmail.com>
2023-07-09 07:16:00 +0300

    created Platform Dto

---
+++ dotnet-api/Dto/PlatformDto.cs
@@ -0,0 +1,15 @@
+namespace dotnet_api.Dto
+{
+    public class PlatformDto
+
+    {
+     
+     public int Id {get; set;}
+
+     public string Name {get; set;}
+
+     public DateTime ReleaseDate {get;set;}
+
+
+    }
+}

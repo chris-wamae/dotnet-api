3d656d596013ef12bb53f5529cfe7828850fabe5
chris-wamae <chriswamae123@gmail.com>
2023-07-10 09:36:55 +0300

    Created Pro player interface

---
+++ dotnet-api/Interfaces/IPro_playerRepository.cs
@@ -0,0 +1,15 @@
+using dotnet_api.Models;
+
+namespace dotnet_api.Interfaces
+{
+  
+  public interface IPro_player
+  {
+    ICollection<Pro_player> GetPro_Players();
+    Pro_player GetPro_PlayerById(int proId);
+    Pro_player GetPro_PlayerByName(string name);
+    bool ProExists(int proId);
+
+  }
+
+}

43ce9ad29c55508fa2f511ecfabba1e3c4b90c3b
chris-wamae <chriswamae123@gmail.com>
2023-07-10 09:55:31 +0300

    Created ProPlayer Dto

---
+++ dotnet-api/Dto/Pro_PlayerDto.cs
@@ -0,0 +1,14 @@
+namespace dotnet_api.Dto
+
+{
+
+ public class Pro_playerDto
+ {
+    public int Id {get;set;}
+
+    public string Name {get;set;}
+
+    public DateTime StartDate {get;set;}
+ }
+
+}
